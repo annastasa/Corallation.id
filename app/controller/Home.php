f798b9813ef0b1785a3b8cf5392c316db415900a
annastasa <71929867+annastasa@users.noreply.github.com>
2021-06-16 20:38:41 +0700

    Delete app directory

--- app/controller/Home.php
+++
@@ -1,8 +0,0 @@
-<?php
-$query = mysqli_query($conn,"SELECT jumlah_donasi from donasi");
-$jumlah_donasi = 0; 
-while($donasi = mysqli_fetch_assoc($query)){
-  $jumlah_donasi += $donasi['jumlah_donasi'];
-}
-
-?>
